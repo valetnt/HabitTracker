6ce3005b1b8ebf27a2ad586f90d24167cff8d8ab
valetnt <valetnt@gmail.com>
2017-07-02 18:50:38 +0200

    fixed some bugs

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -16,22 +17,48 @@ import android.widget.TextView;
 import com.example.android.habittracker.data.HabitTrackerContract.HabitEntry;
 import com.example.android.habittracker.data.HabitTrackerDbHelper;
 
+/*
+ * This activity allows the user to keep track daily of the number of hours he has spent
+ * doing a certain activity, such as swimming, jogging, studying French,
+ * or helping his poor old grandma buying grocery and doing the housework.
+ * NO CALENDARS have been used for the moment, for the sake of simplicity.
+ * It is simply assumed that the app is used every day, so that when you re-launch the app,
+ * and so this activity (which is the ONLY activity) is recreated, the weekday that is displayed
+ * at the top of the screen is simply the next day.
+ */
 public class MainActivity extends AppCompatActivity {
 
     HabitTrackerDbHelper mDbHelper;
+
+    // Spinners for selecting the number of hours spent doing each activity:
     Spinner mSpinnerJogging;
     Spinner mSpinnerSwimming;
     Spinner mSpinnerGranma;
     Spinner mSpinnerFrench;
-    TextView mWeekday;
-    int mCurrentWeek;
-    Button mButtonNextDay;
+    // There is a button to confirm that data has been entered correctly,
+    // and that we want the database to update with this new piece of data.
+    // After you have confirmed, you WILL NOT be able to edit data!
+    // In other words, you are done for the current day.
     Button mButtonOk;
-    LinearLayout mSummaryTable;
+    /*
+     * To make things simple (so we don't need to use calendars for the moment),
+     * when we install the app, the weekday is initially set to MONDAY
+     * and the week is initially set to WEEK 1.
+     * Then, if the activity is recreated, the day that is going to be displayed
+     * is the next day (i.e. Tuesday of week 1).
+     * And so on, every time the activity is recreated the weekday increments by 1, and once we
+     * reach Sunday, it starts again from day 1 (= Monday) and the week increments by 1.
+     */
+    int mCurrentWeek;
+    int mCurrentWeekday;
+    // There is also a button that allows to skip directly to the next day.
+    // This is just for convenience, to make it quicker to test app behaviour!
+    // This button has the same effect as closing and then relaunching the app.
+    Button mButtonNextDay;
 
-    // For the moment, we don't have an editor, so the user can only enter data once.
-    // Make sure that he can hit "CONFIRM" button ONLY ONCE.
-    boolean mIsConfirmed = false;
+    TextView mWeekday; // To be displayed at the top of the screen
+
+    LinearLayout mSummaryTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
         mSpinnerSwimming = (Spinner) findViewById(R.id.spinner_swimming);
         mSpinnerGranma = (Spinner) findViewById(R.id.spinner_granma);
         mSpinnerFrench = (Spinner) findViewById(R.id.spinner_french);
-
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.spinner_time_options, android.R.layout.simple_spinner_item);
@@ -57,25 +83,9 @@ public class MainActivity extends AppCompatActivity {
         mSpinnerFrench.setAdapter(adapter);
 
         mWeekday = (TextView) findViewById(R.id.weekday);
-        mWeekday.setText(getString(R.string.monday));
 
-        // Update week index
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        String[] columns = {HabitEntry.COLUMN_WEEK};
-        Cursor cursor = db.query(HabitEntry.TABLE_NAME, columns, null, null, null, null,
-                null);
-        try {
-            if (cursor.getCount() == 0) {
-                // Initialize the week index the very first time this app is used
-                mCurrentWeek = 1;
-            } else {
-                cursor.moveToLast();
-                mCurrentWeek = cursor.getInt(cursor.getColumnIndex(HabitEntry.COLUMN_WEEK));
-            }
-        } finally {
-            cursor.close();
-        }
-
+        // Update weekday (and possibly week) every time this activity is recreated
+        updateWeekDay();
 
         mSummaryTable = (LinearLayout) findViewById(R.id.table);
         displaySummary();
@@ -91,76 +101,150 @@ public class MainActivity extends AppCompatActivity {
                 mSpinnerGranma.setSelection(0);
                 mSpinnerFrench.setSelection(0);
 
-                // Update the day of the week
-                if(mWeekday.getText().toString().equals(getString(R.string.monday))) {
-                    mWeekday.setText(getString(R.string.tuesday));
-                } else if(mWeekday.getText().toString().equals(getString(R.string.tuesday))) {
-                    mWeekday.setText(getString(R.string.wednesday));
-                } else if(mWeekday.getText().toString().equals(getString(R.string.wednesday))) {
-                    mWeekday.setText(getString(R.string.thursday));
-                } else if(mWeekday.getText().toString().equals(getString(R.string.thursday))) {
-                    mWeekday.setText(getString(R.string.friday));
-                } else if(mWeekday.getText().toString().equals(getString(R.string.friday))) {
-                    mWeekday.setText(getString(R.string.saturday));
-                } else if(mWeekday.getText().toString().equals(getString(R.string.saturday))) {
-                    mWeekday.setText(getString(R.string.sunday));
-                } else if(mWeekday.getText().toString().equals(getString(R.string.sunday))) {
-                    mWeekday.setText(getString(R.string.monday));
-                    // With the beginning of a new week, increment the number of the week by 1
-                    int lastWeek;
-                    SQLiteDatabase db = mDbHelper.getReadableDatabase();
-                    String[] columns = {HabitEntry.COLUMN_WEEK};
-                    Cursor cursor = db.query(HabitEntry.TABLE_NAME, columns, null, null, null, null,
-                            null);
-                    try {
-                        cursor.moveToLast(); // Read last week index
-                        lastWeek = cursor.getInt(cursor.getColumnIndex(HabitEntry.COLUMN_WEEK));
-                    } finally {
-                        cursor.close();
-                    }
-                    // Increment week number by 1
-                    mCurrentWeek = lastWeek + 1;
-                }
+                // Update weekday (and possibly week)
+                updateWeekDay();
+
+                // Reactivate the "CONFIRM" button
+                mButtonOk.setEnabled(true);
+
+                // Disable the "NEXT DAY" button until the database is updated
+                // with the current day data
+                mButtonNextDay.setEnabled(false);
             }
         });
+
+        // Disable the "NEXT DAY" button until the database is updated
+        // with the current day data
+        mButtonNextDay.setEnabled(false);
 
         mButtonOk = (Button) findViewById(R.id.button_ok);
         mButtonOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (!mIsConfirmed) {
-                    // Make it impossible for the user to hit "CONFIRM" more than once
-                    mIsConfirmed = true;
+                // Store the number of hours spent doing each activity for the current day
+                // into an integer variable, reading from the spinner selected items
+                int todayJogging = getSelectedInteger(mSpinnerJogging.getSelectedItemPosition());
+                int todaySwimming = getSelectedInteger(mSpinnerSwimming.getSelectedItemPosition());
+                int todayGranma = getSelectedInteger(mSpinnerGranma.getSelectedItemPosition());
+                int todayFrench = getSelectedInteger(mSpinnerFrench.getSelectedItemPosition());
 
-                    // Store the number of hours spent doing each activity for the current day
-                    // into an integer variable, reading from the spinner selected items
-                    int todayJogging = getSelectedInteger(mSpinnerJogging.getSelectedItemPosition());
-                    int todaySwimming = getSelectedInteger(mSpinnerSwimming.getSelectedItemPosition());
-                    int todayGranma = getSelectedInteger(mSpinnerGranma.getSelectedItemPosition());
-                    int todayFrench = getSelectedInteger(mSpinnerFrench.getSelectedItemPosition());
+                // Update database
+                SQLiteDatabase db = mDbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put(HabitEntry.COLUMN_WEEK, mCurrentWeek);
+                values.put(HabitEntry.COLUMN_DAY, mCurrentWeekday);
+                values.put(HabitEntry.COLUMN_JOGGING_TIME, todayJogging);
+                values.put(HabitEntry.COLUMN_SWIMMING_TIME, todaySwimming);
+                values.put(HabitEntry.COLUMN_GRANMA_TIME, todayGranma);
+                values.put(HabitEntry.COLUMN_FRENCH_TIME, todayFrench);
+                db.insert(HabitEntry.TABLE_NAME, null, values);
 
-                    // Update database
-                    SQLiteDatabase db = mDbHelper.getWritableDatabase();
-                    ContentValues values = new ContentValues();
-                    values.put(HabitEntry.COLUMN_WEEK, mCurrentWeek);
-                    values.put(HabitEntry.COLUMN_JOGGING_TIME, todayJogging);
-                    values.put(HabitEntry.COLUMN_SWIMMING_TIME, todaySwimming);
-                    values.put(HabitEntry.COLUMN_GRANMA_TIME, todayGranma);
-                    values.put(HabitEntry.COLUMN_FRENCH_TIME, todayFrench);
-                    db.insert(HabitEntry.TABLE_NAME, null, values);
+                // Update summary table with the newly entered data
+                displaySummary();
 
-                    // Update summary table with the newly entered data
-                    displaySummary();
-                }
+                // Make it impossible for the user to hit "CONFIRM" more than once
+                mButtonOk.setEnabled(false);
+
+                // Enable the "NEXT DAY" button
+                mButtonNextDay.setEnabled(true);
             }
         });
+    }
+
+    // Helper method that updates week and day of the week.
+    // To be invoked every time the activity is recreated, AND
+    // when the button "NEXT DAY" is pressed.
+    private void updateWeekDay() {
+
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        String[] columns = {HabitEntry.COLUMN_WEEK, HabitEntry.COLUMN_DAY};
+        Cursor cursor = db.query(HabitEntry.TABLE_NAME, columns, null, null, null, null,
+                null);
+        try {
+            if (cursor.getCount() == 0) {
+                // Initialize the week index the very first time this app is used
+                mCurrentWeek = 1;
+                // Let's assume that we start using this app for the first time on a Monday
+                mCurrentWeekday = HabitEntry.MON;
+            } else {
+                // If it is not the first time we use the app, then check
+                // when it was the last time it's been used and increment day by 1.
+                // For the moment, we are under the simplified assumption that the app
+                // is used EVERY DAY. No calendars have been used, in order to keep things
+                // as simple as possible.
+                cursor.moveToLast();
+                int weekLastUsed = cursor.getInt(cursor.getColumnIndex(HabitEntry.COLUMN_WEEK));
+                int dayLastUsed = cursor.getInt(cursor.getColumnIndex(HabitEntry.COLUMN_DAY));
+                if (dayLastUsed == HabitEntry.SUN) {
+                    // If last time was a Sunday, today it is the Monday of the next week
+                    mCurrentWeek = weekLastUsed + 1;
+                    mCurrentWeekday = HabitEntry.MON;
+                } else {
+                    mCurrentWeek = weekLastUsed;
+                    mCurrentWeekday = dayLastUsed + 1;
+                }
+            }
+
+            mWeekday.setText(getWeekday(mCurrentWeekday, 2));
+
+        } finally {
+            cursor.close();
+        }
+    }
+
+    /**
+     * Helper method that returns the weekday name to be displayed in the summary table
+     *
+     * @param constant: one of the constants defined in {@link HabitTrackerDbHelper}
+     * @param flag:     if flag == 1, it returns the abbreviated version of the name of the weekday;
+     *                  if flag == 2, it returns the full name.
+     */
+    private String getWeekday(int constant, int flag) {
+        switch (flag) {
+            case 1:
+                if (constant == HabitEntry.MON) {
+                    return "Mon";
+                } else if (constant == HabitEntry.TUE) {
+                    return "Tue";
+                } else if (constant == HabitEntry.WED) {
+                    return "Wed";
+                } else if (constant == HabitEntry.THU) {
+                    return "Thu";
+                } else if (constant == HabitEntry.FRI) {
+                    return "Fri";
+                } else if (constant == HabitEntry.SAT) {
+                    return "Sat";
+                } else if (constant == HabitEntry.SUN) {
+                    return "Sun";
+                }
+
+            case 2:
+                if (constant == HabitEntry.MON) {
+                    return "Monday";
+                } else if (constant == HabitEntry.TUE) {
+                    return "Tuesday";
+                } else if (constant == HabitEntry.WED) {
+                    return "Wednesday";
+                } else if (constant == HabitEntry.THU) {
+                    return "Thursday";
+                } else if (constant == HabitEntry.FRI) {
+                    return "Friday";
+                } else if (constant == HabitEntry.SAT) {
+                    return "Saturday";
+                } else if (constant == HabitEntry.SUN) {
+                    return "Sunday";
+                }
+
+            default:
+                return null;
+        }
     }
 
     // Helper method that returns the integer value corresponding to the item
     // selected in the spinner dropdown menu
     private int getSelectedInteger(int position) {
-        switch(position) {
+        switch (position) {
             case 1:
                 return HabitEntry.NONE;
             case 2:
@@ -175,36 +259,36 @@ public class MainActivity extends AppCompatActivity {
         return 0;
     }
 
-    // Helper method to update and display the summary table
+    // Helper method to update and display the summary table for the current week
     private void displaySummary() {
         // Build up the table retrieving data from database
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        String[] columns = {HabitEntry.COLUMN_JOGGING_TIME, HabitEntry.COLUMN_SWIMMING_TIME,
-                HabitEntry.COLUMN_GRANMA_TIME, HabitEntry.COLUMN_FRENCH_TIME};
-        String selection = "WHERE " + HabitEntry.COLUMN_WEEK + " =? ";
+        String[] columns = {HabitEntry.COLUMN_DAY, HabitEntry.COLUMN_JOGGING_TIME,
+                HabitEntry.COLUMN_SWIMMING_TIME, HabitEntry.COLUMN_GRANMA_TIME,
+                HabitEntry.COLUMN_FRENCH_TIME};
+        // We are only interested in displaying the current week
+        String selection = HabitEntry.COLUMN_WEEK + " =? ";
         String selectionArgs[] = {String.valueOf(mCurrentWeek)};
         Cursor cursor = db.query(HabitEntry.TABLE_NAME, columns, selection, selectionArgs, null,
                 null, null);
         try {
-            if(cursor.getCount() > 0) {
+            if (cursor.getCount() > 0) {
                 // If there is at least one row, make the table visible
                 mSummaryTable.setVisibility(View.VISIBLE);
 
-                // Loop on all the table entries
+                // Loop on all the table entries, from Monday till today
                 cursor.moveToFirst();
-                while(!cursor.isAfterLast()) {
+                while (!cursor.isAfterLast()) {
 
                     // First, inflate the row layout from a resource file
                     LinearLayout new_entry = (LinearLayout)
                             LayoutInflater.from(this).inflate(R.layout.table_entries,
-                                    mSummaryTable, false);
-                    // Attach the row to the already existing table
-                    mSummaryTable.addView(new_entry);
+                                    mSummaryTable, true);
 
                     // Now, fill the row
                     // Zeroth column: the day of the week
-                    ((TextView) new_entry.findViewById(R.id.weekday)).setText
-                            (abbreviateWeekday(mWeekday.getText().toString()));
+                    int weekday = cursor.getInt(cursor.getColumnIndex(HabitEntry.COLUMN_DAY));
+                    ((TextView) new_entry.findViewById(R.id.weekday)).setText(getWeekday(weekday, 1));
 
                     // First, second, third and fourth columns:
                     // the number of hours spent doing each activity.
@@ -241,27 +325,6 @@ public class MainActivity extends AppCompatActivity {
 
         } finally {
             cursor.close();
-        }
-    }
-
-    // Helper method to abbreviate weekday (e.g. Monday --> Mon)
-    private String abbreviateWeekday(String weekday) {
-        if(weekday.equals(getString(R.string.monday))) {
-            return getString(R.string.mon);
-        } else if(weekday.equals(getString(R.string.tuesday))) {
-            return getString(R.string.tue);
-        } else if(weekday.equals(getString(R.string.wednesday))) {
-            return getString(R.string.wen);
-        } else if(weekday.equals(getString(R.string.thursday))) {
-            return getString(R.string.thu);
-        } else if(weekday.equals(getString(R.string.friday))) {
-            return getString(R.string.fri);
-        } else if(weekday.equals(getString(R.string.saturday))) {
-            return getString(R.string.sat);
-        } else if(weekday.equals(getString(R.string.sunday))) {
-            return getString(R.string.sun);
-        } else {
-            return null;
         }
     }
 }
--- app/src/main/java/com/example/android/habittracker/data/HabitTrackerContract.java
+++ app/src/main/java/com/example/android/habittracker/data/HabitTrackerContract.java
@@ -9,18 +9,29 @@ public class HabitTrackerContract {
     public static class HabitEntry implements BaseColumns {
         // Table name
         public static final String TABLE_NAME = "habits";
+
         // Columns
         public static final String COLUMN_ID = BaseColumns._ID;
         public static final String COLUMN_WEEK = "week";
+        public static final String COLUMN_DAY = "weekday";
         public static final String COLUMN_JOGGING_TIME = "jogging";
         public static final String COLUMN_SWIMMING_TIME = "swimming";
         public static final String COLUMN_GRANMA_TIME = "granma";
         public static final String COLUMN_FRENCH_TIME = "french";
+
         // Other constants
         public static final int NONE = 0;
         public static final int HALF_HOUR = 1;
         public static final int ONE_HOUR = 2;
         public static final int ONE_HOUR_AND_HALF = 3;
         public static final int TWO_HOURS = 4;
+
+        public static final int MON = 1;
+        public static final int TUE = 2;
+        public static final int WED = 3;
+        public static final int THU = 4;
+        public static final int FRI = 5;
+        public static final int SAT = 6;
+        public static final int SUN = 7;
     }
 }
