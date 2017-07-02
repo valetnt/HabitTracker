a9d3a5cffdf6b90835f4c2d034da95b1f74e815e
valetnt <valetnt@gmail.com>
2017-07-02 19:29:40 +0200

    improved code

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -22,9 +22,10 @@ import com.example.android.habittracker.data.HabitTrackerDbHelper;
  * doing a certain activity, such as swimming, jogging, studying French,
  * or helping his poor old grandma buying grocery and doing the housework.
  * NO CALENDARS have been used for the moment, for the sake of simplicity.
- * It is simply assumed that the app is used every day, so that when you re-launch the app,
- * and so this activity (which is the ONLY activity) is recreated, the weekday that is displayed
- * at the top of the screen is simply the next day.
+ * It is simply assumed that the app IS USED EVERY DAY AND ONLY ONCE PER DAY,
+ * so that when you relaunch the app -- and so this activity is recreated --
+ * the weekday that is displayed at the top of the screen is simply the next day.
+ *
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -37,22 +38,25 @@ public class MainActivity extends AppCompatActivity {
     Spinner mSpinnerSwimming;
     Spinner mSpinnerGranma;
     Spinner mSpinnerFrench;
+
     // There is a button to confirm that data has been entered correctly,
     // and that we want the database to update with this new piece of data.
     // After you have confirmed, you WILL NOT be able to edit data!
     // In other words, you are done for the current day.
     Button mButtonOk;
+
     /*
      * To make things simple (so we don't need to use calendars for the moment),
      * when we install the app, the weekday is initially set to MONDAY
      * and the week is initially set to WEEK 1.
      * Then, if the activity is recreated, the day that is going to be displayed
      * is the next day (i.e. Tuesday of week 1).
-     * And so on, every time the activity is recreated the weekday increments by 1, and once we
-     * reach Sunday, it starts again from day 1 (= Monday) and the week increments by 1.
+     * And so on, every time the activity is recreated, the weekday increments by 1,
+     * and once we reach Sunday, it starts again from day 1 (Monday) and the week increments by 1.
      */
     int mCurrentWeek;
     int mCurrentWeekday;
+
     // There is also a button that allows to skip directly to the next day.
     // This is just for convenience, to make it quicker to test app behaviour!
     // This button has the same effect as closing and then relaunching the app.
@@ -60,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mWeekday; // To be displayed at the top of the screen
 
-    LinearLayout mSummaryTable;
+    LinearLayout mSummaryTable; // A summary of the current week
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,13 +90,17 @@ public class MainActivity extends AppCompatActivity {
 
         mWeekday = (TextView) findViewById(R.id.weekday);
 
-        // Update weekday (and possibly week) every time this activity is recreated
+        // Update day (and possibly week) every time this activity is recreated
         updateWeekDay();
         Log.i(LOG_TAG, "Current week: " + mCurrentWeek);
         Log.i(LOG_TAG, "Current weekday: " + mCurrentWeekday);
 
         mSummaryTable = (LinearLayout) findViewById(R.id.table);
         displaySummary();
+
+        // Disable the "NEXT DAY" button until the database is updated
+        // with the data of the current day
+        mButtonNextDay.setEnabled(false);
 
         mButtonNextDay = (Button) findViewById(R.id.button_next_day);
         mButtonNextDay.setOnClickListener(new View.OnClickListener() {
@@ -105,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
                 mSpinnerGranma.setSelection(0);
                 mSpinnerFrench.setSelection(0);
 
-                // Update weekday (and possibly week)
+                // Update day (and possibly week)
                 updateWeekDay();
                 Log.i(LOG_TAG, "Current week: " + mCurrentWeek);
                 Log.i(LOG_TAG, "Current weekday: " + mCurrentWeekday);
@@ -114,14 +122,10 @@ public class MainActivity extends AppCompatActivity {
                 mButtonOk.setEnabled(true);
 
                 // Disable the "NEXT DAY" button until the database is updated
-                // with the current day data
+                // with the data of the current day
                 mButtonNextDay.setEnabled(false);
             }
         });
-
-        // Disable the "NEXT DAY" button until the database is updated
-        // with the current day data
-        mButtonNextDay.setEnabled(false);
 
         mButtonOk = (Button) findViewById(R.id.button_ok);
         mButtonOk.setOnClickListener(new View.OnClickListener() {
@@ -129,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 // Store the number of hours spent doing each activity for the current day
-                // into an integer variable, reading from the spinner selected items
+                // into an integer variable, reading from the spinner selected item
                 int todayJogging = getSelectedInteger(mSpinnerJogging.getSelectedItemPosition());
                 int todaySwimming = getSelectedInteger(mSpinnerSwimming.getSelectedItemPosition());
                 int todayGranma = getSelectedInteger(mSpinnerGranma.getSelectedItemPosition());
@@ -158,9 +162,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Helper method that updates week and day of the week.
-    // To be invoked every time the activity is recreated, AND
-    // when the button "NEXT DAY" is pressed.
+    // Helper method that updates day (and possibly week).
+    // It is invoked every time the activity is recreated, AND
+    // when the button "NEXT DAY" is clicked.
     private void updateWeekDay() {
 
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
@@ -168,11 +172,13 @@ public class MainActivity extends AppCompatActivity {
         Cursor cursor = db.query(HabitEntry.TABLE_NAME, columns, null, null, null, null,
                 null);
         try {
+
             if (cursor.getCount() == 0) {
                 // Initialize the week index the very first time this app is used
                 mCurrentWeek = 1;
                 // Let's assume that we start using this app for the first time on a Monday
                 mCurrentWeekday = HabitEntry.MON;
+
             } else {
                 // If it is not the first time we use the app, then check
                 // when it was the last time it's been used and increment day by 1.
@@ -267,6 +273,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Helper method to update and display the summary table for the current week
     private void displaySummary() {
+
         // Build up the table retrieving data from database
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
         String[] columns = {HabitEntry.COLUMN_DAY, HabitEntry.COLUMN_JOGGING_TIME,
@@ -278,6 +285,7 @@ public class MainActivity extends AppCompatActivity {
         Cursor cursor = db.query(HabitEntry.TABLE_NAME, columns, selection, selectionArgs, null,
                 null, null);
         try {
+
             if (cursor.getCount() > 0) {
                 // If there is at least one row, make the table visible
                 mSummaryTable.setVisibility(View.VISIBLE);
@@ -324,6 +332,7 @@ public class MainActivity extends AppCompatActivity {
 
                     cursor.moveToNext();
                 }
+
             } else {
                 // Table remains empty, so hide it
                 mSummaryTable.setVisibility(View.GONE);
